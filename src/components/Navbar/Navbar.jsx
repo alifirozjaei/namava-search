a760cf206db6e14eb28f51757da5db24f8ade041
Ali L Firozjaeai <ali.firozjaeii@gmail.com>
2023-10-04 15:02:01 +0330

    Fix Navbar Scroll

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Navbar.module.css";
 import NamavaIcon from "../Icons/NamavaIcon.jsx";
 import SearchIcon from "../Icons/SearchIcon.jsx";
 import MessageIcon from "../Icons/MessageIcon.jsx";
 const Navbar = () => {
+  const [top, setTop] = useState(0);
+  useEffect(() => {
+    let prevScrollpos = window.pageYOffset;
+    const onScroll = (e) => {
+      let currentScrollPos = window.pageYOffset;
+      if (prevScrollpos > currentScrollPos) {
+        // if scroll up show navbar
+        setTop(0);
+      } else {
+        // if scroll down hide navbar
+        setTop("-100px");
+      }
+      prevScrollpos = currentScrollPos;
+    };
+    window.addEventListener("scroll", onScroll);
+    return () => {
+      removeEventListener("scroll", onScroll);
+    };
+  }, [top]);
+
   return (
-    <nav className={styles.navbar}>
+    <nav className={styles.navbar} style={{ top: top }}>
       <div className={styles["navbar-link"]}>
         <NamavaIcon />
         <span className={styles["navbar-link-item"]}>خانه</span>
